2398c875a072b45bb48f338dc1196639bcf77c64
Babinbinbin <127527046+Babinbinbin@users.noreply.github.com>
2023-05-20 18:39:02 +0530

    registering users works now

--- codeaway/users/urls.py
+++ codeaway/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('register2',views.register2,name ="register")
+    path('register',views.register,name ="register"),
+    path("login", views.login, name="login"),
+    path('registerhomepage',views.registerhomepage,name="registerhomepage")
 ]
