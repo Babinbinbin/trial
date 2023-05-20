df33e2f9498f621f068870493257e9aae678551d
cvbshcbad <121386890+cvbshcbad@users.noreply.github.com>
2023-05-20 18:58:25 +0530

    update register

--- codeaway/users/views.py
+++ codeaway/users/views.py
@@ -2,17 +2,35 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .form import UserRegistrationForm
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 
-def register2(request):
-    if request.method == "POST":
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}')
-            return redirect('index')
-    else:
-        form = UserRegistrationForm()
-    return render(request,'users/register2.html',{'form':form})
+def registerhomepage(request):
+    pass
+    
+    
 
+def login(request):
+    if request.method=="POST":
+        username=request.POST["username"]
+        email=request.POST["email"]
+        password=request.POST["password"]
+        
+    return render(request, "users/register2.html")
+
+def register(request):
+    if request.method=="POST":
+        username=request.POST["username"]
+        email=request.POST["email"]
+        password=request.POST["password"]
+        user=User.objects.create_user(username,email,password)
+        user.user_name=username
+        user.save()
+        messages.success(request, "Account created successfully")
+    return render(request,"users/register2.html")
+         
+                
