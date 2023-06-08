056a27b8d68c6fe12dc091397644403e9b530922
Babinbinbin <127527046+Babinbinbin@users.noreply.github.com>
2023-06-08 22:23:05 +0530

    added new classes

--- codeaway/first/admin.py
+++ codeaway/first/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-
+from .models import Article, Content
 # Register your models here.
+class ContentInline(admin.TabularInline):
+    model= Content
+class ArticleAdmin(admin.ModelAdmin):
+    inlines= [ContentInline]
+
+admin.site.register(Article)
+admin.site.register(Content)
  
